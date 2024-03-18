6bd8d1d15c891a340325e3bd20cc8e7b4c828e98
s26784 <s26784@pjwstk.edu.pl>
2024-03-18 11:32:29 +0100

    Deloading and Loading methods implementation

---
+++ ConsoleApp1/ConsoleApp1/Models/Base/Container.cs
@@ -0,0 +1,38 @@
+﻿using ConsoleApp1.SpecialExceptions;
+
+namespace ConsoleApp1.Models.Base;
+
+public abstract class Container
+{
+    protected double LoadWeight { get; set; }
+    protected double Height{ get; set; }
+    protected double CurbWeight{ get; set; }
+    protected double Depth{ get; set; }
+    protected string SerialNumber{ get; set; }
+    protected double LoadCapacity{ get; set; }
+
+
+    protected Container(double loadWeight, double height, double curbWeight, double depth, string serialNumber,
+        double loadCapacity)
+    {
+        this.LoadWeight = loadWeight;
+        this.Height = height;
+        this.CurbWeight = curbWeight;
+        this.Depth = depth;
+        this.SerialNumber = serialNumber;
+        this.LoadCapacity = loadCapacity;
+    }
+
+    public void Deload()
+    {
+        this.LoadWeight = 0;
+    }
+
+    public void AddLoad(double loadToAdd)
+    {
+        if (loadToAdd + LoadWeight > LoadCapacity) throw new OverfillException("You have overloaded container");
+        LoadWeight += loadToAdd;
+    } 
+    
+    
+}
